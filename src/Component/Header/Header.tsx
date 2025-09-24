fc1210e8a820498bc0aac32ea943f9d543e3ec40
Ychnightder <pierreychnightder561@gmail.com>
2025-09-24 16:46:45 +0200

    fin composant link

--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -12,22 +12,18 @@ import Link from "./link";
 		return (
 			<header className="flex justify-center items-center">
 				<div className="h-[110px] w-5/6 flex flex-row items-center justify-between border-b border-grayish-blue">
-					<div className="flex flex-row justify-center items-center gap-10">
+					<div className="flex flex-row  w-2/4  h-full items-center gap-10">
 						<a href="index.html" className="flex items-center mb-2">
-							<img src="/public/images/logo.svg" alt="Sneaker_logo" className="img_logo" />
+							<img src="/images/logo.svg" alt="Sneaker_logo" className="img_logo" />
 						</a>
 
 						<nav className="ml-10 flex space-x-8 gap-8">
 							{navItems.map((item, index) => {
-								const element = navItems[index];
-								return (
-									<Link key={index} href={element.href} onClick={undefined} active={undefined}>
-										{element.name}
-									</Link>
-								);
+								return <Link key={index} href={item.href} onClick={undefined} active={undefined} children={item.name} />;
 							})}
 						</nav>
 					</div>
+					<div className="flex flex-row justify-center items-center w-2/4 h-full gap-6 bg-OrangeV"></div>
 				</div>
 			</header>
 		);
--- src/Component/Header/link.tsx
+++ src/Component/Header/link.tsx
@@ -9,16 +9,13 @@ type Props = {
 };
 
 export default function Link({ href = '#', children, onClick, active = false, className = '' }: Props): React.JSX.Element {
-	const base = 'inline-flex items-center text-sm font-base transition-all duration-200 h-full border-transparent  ';
+	const base = 'inline-flex items-center justify-center text-sm font-base transition-all duration-200 h-full  ';
 
-	const normal = 'text-grayish-blue hover:text-very-dark-blue hover:border-OrangeV border-b-4';
-
-	const activeCls = 'text-very-dark-blue border-OrangeV';
-
-	const classes = [base, active ? activeCls : normal, className].filter(Boolean).join(' ');
+	const normal = 'text-grayish-blue hover:text-very-dark-blue  hover:inset-shadow-link';
+	const classes = [base, normal, className].filter(Boolean).join(' ');
 
 	return (
-		<div className="flex flex-row items-center h-[110px]">
+		<div className="flex flex-row items-center justify-center h-[110px] ">
 			<a href={href} onClick={onClick} aria-current={active ? 'page' : undefined} className={classes}>
 				{children}
 			</a>
